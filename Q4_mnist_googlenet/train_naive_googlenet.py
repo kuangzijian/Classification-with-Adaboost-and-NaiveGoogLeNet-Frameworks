e9b177f85e06989d28be32dcdaf53eb1eaa3af23
kuangzijian <kuangzijian1@hotmail.com>
2020-10-08 23:44:42 -0600

    modified the GoogLeNet.py and NaiveGoogLeNet.py to accept multiple chanel input as variable

--- Q4_mnist_googlenet/train_naive_googlenet.py
+++ Q4_mnist_googlenet/train_naive_googlenet.py
@@ -17,6 +17,7 @@ def train(args, model, device, train_loader, optimizer, epoch):
         optimizer.zero_grad()
         model.cuda()
         output = model(data)
+        output = F.log_softmax(output[0], dim=1)
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
@@ -31,6 +32,7 @@ def train(args, model, device, train_loader, optimizer, epoch):
     m, s = divmod(remainder, 60)
     time_str = '%02d:%02d:%02d' % (h, m, s)
     print('\nTime spent: ', time_str)
+
 def test(model, device, test_loader):
     model.eval()
     test_loss = 0
@@ -57,7 +59,7 @@ def main():
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=3, metavar='N',
+    parser.add_argument('--epochs', type=int, default=2, metavar='N',
                         help='number of epochs to train (default: 14)')
     parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
                         help='learning rate (default: 1.0)')
@@ -98,7 +100,7 @@ def main():
     train_loader = torch.utils.data.DataLoader(dataset1,**kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **kwargs)
 
-    model = NaiveGoogLeNet()
+    model = NaiveGoogLeNet(input_channel=1, num_classes=10, init_weights=True)
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
 
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
